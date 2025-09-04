2217b715b39fdb8b6544a89b8a53fd10787233ee
Arif fathur Rahman <arif22ti@mahasiswa.pcr.ac.id>
2025-09-04 10:19:30 +0700

    setup config binding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.sipantau"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.sipantau"
@@ -30,6 +30,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+    buildFeatures{
+        viewBinding = true
+    }
+
     kotlinOptions {
         jvmTarget = "11"
     }
